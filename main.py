d370c16aa22dc8150c529255cb95247c8f06e227
Jared Raga <jarediraga@gmail.com>
2019-03-27 07:48:28 +0000

    Update for Stable Release
    Added custom error logger.
    Downloader tries 5 times before giving up.
    Changed User-agent to bot's name.
    Function removes invalid filename characters.
    
    Gave option for user to specify where to start downloading.

--- main.py
+++ main.py
@@ -1,33 +1,92 @@
-# Beginning Date: 7/31/18 1:15PM
+# Beginning  Date: 7/31/18 1:15PM
+# Completion Date: 7/31/18 1:50PM
 
+# Program Version: 1.0.0
+
+import bs4, sys, os
 import requests
-import bs4
-import os
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+from logger import setup_custom_logger
 
-num_chapters = 439
-save_path = ''
+exit_message = 'Thank you for using Wuxia Novelscraper!'
 
-for i in range(num_chapters):
+# Function that downloads the chapters
+def requests_session(url):
+	''' Tries downloading the page 5 times before giving up '''
+	requests_session = requests.Session()
+	requests_retries = Retry(total=5, backoff_factor=1, status_forcelist=[ 502, 503, 504 ]) # Forces it to retry and ignore common HTTP errors
+	requests_session.mount('http://', HTTPAdapter(max_retries=requests_retries))
+	response = requests_session.get(url, headers={'User-agent':bot_name})
 
-	current_chapter = i + 1
+	return response
 
-	# Get chapter page source from Wuxia World
-	response = requests.get('https://www.wuxiaworld.com/novel/*%s' % current_chapter )
+# Removes invalid filename characters
+def remove_invalid_char(string):
+	for char in string:
+		# Spare those who can be spared	
+		if char == '?':
+			return string.replace('?','')
+		elif char == '"':
+			return string.replace('"',"'")
+		elif char == ':':
+			return string.replace(':','-')
+		# If not, replace them with underscores. It can't be helped
+		for invalid in '" * / : < > ? \\ |'.split():
+			string = string.replace(invalid,'_')
+		return string
+
+
+''' Start of the program '''
+app_version = '1.0.0'
+logger = setup_custom_logger(app_version)
+bot_name = ('Wuwuro Bot %s' %app_version)
+
+''' These are the changeable variables '''
+save_path = '' # * MUST create a folder with the novel name
+novel_url = '' # * MUST copy the whole URL here. EXCEPT the chapter number
+start_at_chap = 0 # * MUST change this based on where you want to start
+end_at_chap = 0 # * MUST change this based on where you want to END
+''' These are the changeable variables '''
+
+''' Starting the scraper '''
+for current_chapter in range(start_at_chap, end_at_chap + 1):
 	
-	# Used Beautiful Soup to easily extract chapter from the html 
-	soup = bs4.BeautifulSoup(response.text)
+	chapter_url = novel_url + str(current_chapter)
+
+	''' Download the page source, Get the chapter, Log errors '''
+	print('Downloading Chapter-%s (%s)' % (current_chapter, chapter_url))
+	try:
+		response = requests_session(chapter_url)
+	except Exception as e:
+		logger.info('Failed to download %s' % chapter_url, exc_info=True)
+		continue
+
+	''' Parsing the HTML File '''
+	soup = bs4.BeautifulSoup(response.text, 'html.parser')
 	chapter = soup.select('.fr-view p')
-	chapter_title = chapter[0].text
+	chapter_title = chapter[1].text
+	print('Downloaded %s' % chapter_title)
 
-	# Save the chapter to a txt file
+
+	''' Saving it to a text file '''
 	filename = chapter_title + '.txt'
+	filename = remove_invalid_char(filename)
 	absolute_path = os.path.join(save_path, filename)
+	
+	try:
+		# For paragraph in chapter
+		for para in chapter:
+			with open(absolute_path,'a+') as f:
+				f.writelines(para.text)
+				# Retain the previous format. Empty line after each paragraph
+				f.writelines('\n')
+				f.writelines('')
+				f.writelines('\n')
+	except Exception as e:
+		logger.error('Failed to save to %s' % filename, exc_info=True)
 
-	for para in chapter:
-		with open(absolute_path,'a+') as f:
-			f.writelines(para.text)
-			f.writelines('\n')
-			f.writelines('')
-			f.writelines('\n')
+	print("Saved to '%s' " % filename)
+	print()
 
-break
+print(exit_message)
